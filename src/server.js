bca92ecc4b6b6b703bf1ad979bdd67232feed8be
Elian Becali <elianbecaliaguiar@gmail.com>
2020-07-13 21:08:37 -0400

    finalizado o 4° dia :rocket:

--- src/server.js
+++ src/server.js
@@ -1,5 +1,30 @@
 const express = require("express")
 const server = express()
 
+
+// configurar pasta pública
+server.use(express.static("public"))
+
+// Utilizamos template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+// configurar caminhos da minha aplicação
+// Página inicial
+// req: Requisição
+// res: Resposta
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+
 //Ligar o servidor
 server.listen(3000)
